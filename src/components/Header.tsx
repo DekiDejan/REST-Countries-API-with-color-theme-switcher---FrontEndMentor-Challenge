5073fa1ba0e29861e169ac9e4da9e57ad137b811
Dejan Mironski Panchevski <99611699+DekiDejan@users.noreply.github.com>
2024-09-03 11:14:21 +0200

    finish dark theme settings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,25 +5,23 @@ import moonSolidIcon from "./assets/moon-solid.svg";
 const Header = () => {
   const [isDark, setIsDark] = useState(false);
 
-  console.log(isDark);
-
   const handleThemeChange = () => {
-    document.documentElement.classList.toggle("dark");
-    if (document.documentElement.classList.contains("dark")) {
-      setIsDark(true);
-    } else setIsDark(false);
+    const documentClass = document.documentElement.classList;
+
+    documentClass.toggle("dark");
+    documentClass.contains("dark") ? setIsDark(true) : setIsDark(false);
   };
 
   return (
-    <header className="flex justify-between items-center px-4 py-8 bg-white dark:bg-dark-blue shadow-md">
+    <header className="flex justify-between items-center px-4 md:px-20 py-8 bg-white dark:bg-dark-blue shadow-md">
       <div>
-        <span className="font-extrabold">Where in the world?</span>
+        <span className="font-extrabold md:text-xl">Where in the world?</span>
       </div>
-      <div onClick={handleThemeChange} className="flex gap-2">
+      <div onClick={handleThemeChange} className="flex gap-2 cursor-pointer">
         <img
-          width={16}
-          height={16}
-          src={isDark ? moonSolidIcon : moonRegularIcon}
+          width={14}
+          height={14}
+          src={isDark ? moonRegularIcon : moonSolidIcon}
           alt="Theme Icon"
           className=""
         />
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        "dark-blue": "hsl(209, 23%, 22%)", // Dark Mode Elements
-        "very-dark-blue-bg-dark": "hsl(207, 26%, 17%)", // Dark Mode Background
         "very-dark-blue-text-light": "hsl(200, 15%, 8%)", // Light Mode Text
         "dark-gray": "hsl(0, 0%, 52%)", // Light Mode Input
+        "dark-blue": "hsl(209, 23%, 22%)", // Dark Mode Elements
+        "very-dark-blue-bg-dark": "hsl(207, 26%, 17%)", // Dark Mode Background
         "very-light-gray-bg-light": "hsl(0, 0%, 98%)", // Light Mode Background
       },
     },
