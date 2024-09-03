27cc255442379e6967934566c4754b1db5be4f1c
Dejan Mironski Panchevski <99611699+DekiDejan@users.noreply.github.com>
2024-09-03 08:56:33 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-dark-blue">
+        <p className="text-dark-gray">Coutries</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
