0fc4847fd342718026e184616b68d2a5e10925f6
Dejan Mironski Panchevski <99611699+DekiDejan@users.noreply.github.com>
2024-09-04 11:56:20 +0200

    add filter by continents

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -2,21 +2,23 @@ import { useEffect, useState } from "react";
 import Country from "./Country";
 
 const Countries = () => {
-  const [data, setData] = useState<[]>([]);
+  const [countries, setCountries] = useState<[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
 
   const [searchInput, setSearchInput] = useState<string>("");
+  const [selectedContinent, setSelectedContinent] =
+    useState<string>("Filter by Region");
 
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchCountries = async () => {
       try {
         const response = await fetch("https://restcountries.com/v3.1/all");
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const result = await response.json();
-        setData(result);
+        setCountries(result);
       } catch (err: any) {
         setError(err.message);
       } finally {
@@ -24,7 +26,7 @@ const Countries = () => {
       }
     };
 
-    fetchData();
+    fetchCountries();
   }, []);
 
   if (loading)
@@ -36,24 +38,51 @@ const Countries = () => {
       <div className="text-center mt-40 font-bold text-xl">Error: {error}</div>
     );
 
-  const filteredItems = data.filter((item: any) =>
-    item.name.common.toLowerCase().includes(searchInput.toLowerCase())
-  );
+  const continents = [
+    "Filter by Region",
+    ...new Set(countries.map((country: any) => country.region)),
+  ];
+
+  const filteredCountries = countries.filter((country: any) => {
+    const matchesSearchInput = country.name.common
+      .toLowerCase()
+      .includes(searchInput.toLowerCase());
+    const matchesSelectedContinent =
+      selectedContinent === "Filter by Region" ||
+      country.region === selectedContinent;
+
+    return matchesSearchInput && matchesSelectedContinent;
+  });
 
   return (
     <section className="m-6">
-      <div className="w-80 mx-auto my-4 px-8 py-4 bg-white dark:bg-dark-blue shadow-md rounded-lg">
-        <input
-          className="w-full dark:bg-dark-blue focus:outline-none"
-          placeholder="Search for a country..."
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-        />
+      <div className="w-full my-6 flex flex-col justify-between md:flex-row gap-4">
+        <div className="w-80 max-w-96 mx-auto md:mx-4 px-8 py-4 bg-white dark:bg-dark-blue shadow-md rounded-lg flex grow">
+          <input
+            className="w-full dark:bg-dark-blue focus:outline-none"
+            placeholder="Search for a country..."
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+        </div>
+        <div className="w-80 max-w-96 mx-auto md:mx-4 px-8 py-4 bg-white dark:bg-dark-blue shadow-md rounded-lg flex grow">
+          <select
+            className="w-full  dark:bg-dark-blue focus:outline-none text-slate-400"
+            value={selectedContinent}
+            onChange={(e) => setSelectedContinent(e.target.value)}
+          >
+            {continents.map((continent, index) => (
+              <option key={index} value={continent}>
+                {continent}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
       <div className="flex flex-wrap gap-12 justify-center">
-        {filteredItems.map(
-          (item: any): JSX.Element => (
-            <Country item={item} key={item.name.common} />
+        {filteredCountries.map(
+          (country: any): JSX.Element => (
+            <Country country={country} key={country.name.common} />
           )
         )}
       </div>
--- src/components/Country.tsx
+++ src/components/Country.tsx
@@ -1,34 +1,34 @@
 type CountryProps = {
-  item: any;
+  country: any;
 };
 
-const Country = ({ item }: CountryProps) => {
+const Country = ({ country }: CountryProps) => {
   return (
     <div className="bg-white dark:bg-dark-blue shadow-md w-80 h-96 rounded-lg overflow-hidden cursor-pointer">
       <div className="w-80 h-48">
         <img
-          src={item.flags.png}
-          alt={`${item.name.common} flag`}
+          src={country.flags.png}
+          alt={`${country.name.common} flag`}
           className="w-full h-full object-cover aspect-[2/1]"
         />
       </div>
       <div className="mx-4 my-6">
         <p className="font-bold mb-4">
-          {item.name.common === "North Macedonia"
+          {country.name.common === "North Macedonia"
             ? "Republic of Macedonia"
-            : item.name.common}
+            : country.name.common}
         </p>
         <p>
           <span className="font-bold text-sm">Population: </span>
-          {new Intl.NumberFormat("en-US").format(item.population)}
+          {new Intl.NumberFormat("en-US").format(country.population)}
         </p>
         <p>
           <span className="font-bold text-sm">Region: </span>
-          {item.region}
+          {country.region}
         </p>
         <p>
           <span className="font-bold text-sm">Capital: </span>
-          {item.capital}
+          {country.capital}
         </p>
       </div>
     </div>
